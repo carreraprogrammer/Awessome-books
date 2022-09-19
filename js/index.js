6045ec8bb9f4df4416e3a86536bf532777af0d34
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-19 15:36:39 -0500

    Add book list in html with javascript

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const addBtn = document.getElementById('add-btn');
+const booksList = document.getElementById('Books-List');
+const parser = new DOMParser();
 
-const booksInformation = [];
+const booksInformation = [
+    {
+      title: "BookExample",
+      author: "AuthorExample",
+    },  
+    {
+      title: "BookExample2",
+      author: "AuthorExample2",
+    },
+    {
+      title: "BookExample3",
+      author: "AuthorExample3",
+    }
+];
 
 function BookData(title, author) {
     this.title = title;
@@ -19,7 +34,27 @@ addBtn.addEventListener('click', () => {
     }  
 }
 )
-const book_list = document.createElement('div')
+
+
+const bookString = booksInformation.map((book) => `
+  <div>
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    <button id="remove">Remove</button>
+  </div>
+`)
+
+bookString.forEach(element => {
+   const bookHtml = parser.parseFromString(element, 'text/html').body.firstChild;
+   booksList.append(bookHtml);
+});
+
+
+
+
+
+
+/* const book_list = document.createElement('div')
 
 const Title = document.createElement('p')
 Title.innerText = title.value;
@@ -27,4 +62,4 @@ Title.appendChild(book_list);
 
 const Author = document.createElement('p')
 Author.innerText = author.value;
-Author.appendChild(book_list);
+Author.appendChild(book_list); */
