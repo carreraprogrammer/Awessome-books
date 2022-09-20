cf3ca9b559bfd4ede3035be18e1dfcc37eb8d92a
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-20 13:34:26 -0500

    Add local storage and remove items event

--- js/index.js
+++ js/index.js
@@ -4,62 +4,63 @@ const addBtn = document.getElementById('add-btn');
 const booksList = document.getElementById('Books-List');
 const parser = new DOMParser();
 
-const booksInformation = [
-    {
-      title: "BookExample",
-      author: "AuthorExample",
-    },  
-    {
-      title: "BookExample2",
-      author: "AuthorExample2",
-    },
-    {
-      title: "BookExample3",
-      author: "AuthorExample3",
-    }
-];
+let booksInformation = [];
 
 function BookData(title, author) {
     this.title = title;
     this.author = author;
 }
 
-addBtn.addEventListener('click', () => {
+const showList = () => {
+      booksList.innerHTML = '';
+
+      booksInformation.forEach((book, i)=> {
+        const bookString = `
+          <div id="div-${i}">
+            <p>${book.title}</p>
+            <p>${book.author}</p>
+            <button id="btn-${i}"class="remove">Remove</button>
+          </div>
+          `;
+
+        const bookHtml = parser.parseFromString(bookString, 'text/html').body.firstChild;
+        booksList.appendChild(bookHtml);
+
+        //Here I will add an event for the remove button
+
+        const remove = bookHtml.querySelector('.remove');
+        remove.addEventListener('click', () => {
+             document.getElementById(`div-${i}`).outerHTML = "";
+             booksInformation.splice(i, 1);
+             localStorage.setItem('books', JSON.stringify(booksInformation));
+             showList();
+            }   
+        );
+    })
+ };
+
+
+addBtn.addEventListener('click', (e) => {
     if(title.value.length > 0 && author.value.length > 0) {
+        e.preventDefault();
+         
         booksInformation.push(new BookData(title.value, author.value));
         title.value = "";
         author.value = "";
+        showList();
+        localStorage.setItem('books', JSON.stringify(booksInformation));
     } else {
        alert('Please fill the fields!') 
     }  
-}
-)
+      
+  }
+ )
 
+ if (localStorage.getItem('books') == null) {
+    booksInformation = [];
+ } else {
+    booksInformation = JSON.parse(localStorage.getItem('books'));
+    showList();
+ }
 
-const bookString = booksInformation.map((book) => `
-  <div>
-    <p>${book.title}</p>
-    <p>${book.author}</p>
-    <button id="remove">Remove</button>
-  </div>
-`)
-
-bookString.forEach(element => {
-   const bookHtml = parser.parseFromString(element, 'text/html').body.firstChild;
-   booksList.append(bookHtml);
-});
-
-
-
-
-
-
-/* const book_list = document.createElement('div')
-
-const Title = document.createElement('p')
-Title.innerText = title.value;
-Title.appendChild(book_list);
-
-const Author = document.createElement('p')
-Author.innerText = author.value;
-Author.appendChild(book_list); */
+ // Locar storage
