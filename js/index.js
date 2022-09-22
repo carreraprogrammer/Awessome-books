b2b07974be7ce09f3edbc1eb27b852678a909c5f
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-22 12:06:51 -0500

    Fix Eslint errors

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+// Import th Books Class
+
+import BookData from './Books.js';
+
+// Import LocalStorage
+
+import LocalStorage from './LocalStorage.js';
+
 // define the base variables
 
 const title = document.getElementById('title');
@@ -5,28 +13,17 @@ const author = document.getElementById('author');
 const addBtn = document.getElementById('add-btn');
 const booksList = document.getElementById('Books-List');
 
-// Import th Books Class
-
-import BookData from './Books.js'; 
-
-// Import LocalStorage
-
-import LocalStorage from './LocalStorage.js'; 
-
 // Create Methods to display the Book Data
 
 class Methods {
-
-  static displayBooks () {
-
+  static displayBooks() {
     const books = LocalStorage.getBooks();
     books.forEach((book, i) => Methods.addBookToList(book, i));
     }
-    
-    static addBookToList(book, i) { 
 
+  static addBookToList(book, i) {
     const booksList = document.getElementById('Books-List');
-    const parser = new DOMParser();  
+    const parser = new DOMParser();
     const bookString = `
       <div id="${i}" class="book-field-information">
         <div class="book-information"><p class="title-added">${book.title}</p>
@@ -38,36 +35,35 @@ class Methods {
         </div>
       </div>
       `;  
-      
+ 
     const bookHtml = parser.parseFromString(bookString, 'text/html').body.firstChild;
 
     booksList.appendChild(bookHtml);
   }  
-  
+
   static clearInput() {
     title.value = '';
     author.value = '';
   }
 
   static deleteBook(target) {
-    if(target.classList.contains('remove')) {
+    if (target.classList.contains('remove')) {
       target.parentElement.parentElement.remove();
     }
   }
 }
 
-//Create an event to display the books
+// Create an event to display the books
 
-document.addEventListener("DOMContentLoaded", Methods.displayBooks);
+document.addEventListener('DOMContentLoaded', Methods.displayBooks);
 
-// Create an event to Add the books 
+// Create an event to Add the books
 
 addBtn.addEventListener('click', () => {
 
-
   // Create a new Objet with the input information
   
-  const book = new BookData(author.value, title.value) 
+  const book = new BookData(author.value, title.value);
 
   // Add book to UI
 
@@ -79,14 +75,14 @@ addBtn.addEventListener('click', () => {
 
   // Clear Input
 
-  Methods.clearInput()
+  Methods.clearInput();
 })
-  
+
 // Create an event to remove the book 
 
 booksList.addEventListener('click',(e) => {
     // Remove the book from the list
-    Methods.deleteBook(e.target)
+    Methods.deleteBook(e.target);
 
     //Remove book from the store 
     LocalStorage.removeBooks(e.target.parentElement.previousElementSibling.firstChild.textContent); 
